b298093b4ad8dcfdb1975f139e07240d2fd3a353
mkiang <mathew.kiang@gmail.com>
2015-10-07 14:09:53 -0400

    Put up an EXAMPLE credential file
    Example will always be blank and new .gitignore rules will prevent a
    real credential file from being uploaded.

---
+++ download_script/EXAMPLE download_script_credentials.py
@@ -0,0 +1,11 @@
+# Put your credentials in a copy of this file and save it as
+#  "download_script_credentials.py" (i.e., remove "EXAMPLE" from file name)
+
+# REQUIRED, all these variable must be filled in.
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+S3_BUCKET_NAME = "beiwe2"
+
+# Optional, the script will prompt if these are not filled in:
+STUDY_ID = ""
+STUDY_KEY = ""
