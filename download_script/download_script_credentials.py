4bd2ee44782e3e7a7ceccb6c608a21d478a90e99
mkiang <mathew.kiang@gmail.com>
2015-10-07 12:56:41 -0400

    Ignore credentials file
    A bit of a safety measure so that active credentials are never
    accidentally uploaded.

--- download_script/download_script_credentials.py
+++
@@ -1,8 +0,0 @@
-#REQUIRED, all these variable must be filled i.
-AWS_ACCESS_KEY_ID = ""
-AWS_SECRET_ACCESS_KEY = ""
-S3_BUCKET_NAME = "beiwe2"
-
-# Optional, the script will prompt if these are not filled in:
-STUDY_ID = ""
-STUDY_KEY = ""
