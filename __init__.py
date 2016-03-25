23c29a73015a2b9793bc7ccd83609193f9b75de5
mkiang <mathew.kiang@gmail.com>
2016-03-25 12:12:21 -0400

    Prevent accidentally uploading of API credentials

--- __init__.py
+++ __init__.py
@@ -3,6 +3,5 @@ from __future__ import absolute_import
 
 from beiwedata.basic import *
 from beiwedata.audio import *
-from beiwedata.download_creds import *
 from beiwedata.download import *
-
+# from beiwedata.download_creds import *
