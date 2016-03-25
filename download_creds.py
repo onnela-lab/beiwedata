4704758cf0aaddf50acf4f80a28b7b10ffc24140
mkiang <mathew.kiang@gmail.com>
2016-03-25 00:22:14 -0400

    Forgot to upload credentials

---
+++ download_creds.py
@@ -0,0 +1,10 @@
+# Go to studies.beiwe.org, log in, and click on manage credentials to get your 
+# See `help(make_request)` or the Onnela Lab Github for more documentation.
+ACCESS_KEY = "YOUR ACCESS KEY HERE"
+SECRET_KEY = "YOUR SECRET KEY HERE"
+
+# Below are the credentials for the test study so you can follow along in the
+# IPython Notebook tutorials.
+TEST_STUDY = "56cf16231206f7536acbaf58"
+TEST_ACCESS = "lM4wcHLkkk/4nOBDgw949MEsRzgd/Amr0UpffyL0KGEeb0FBJt813PPVwwrLgoPZ"
+TEST_SECRET = "QHnsu9FjPjOhfRV+VM9VZivhYJ3wpTTNiEWaik04XKXFfn/pjvdSIAOw+rWAWweS"
